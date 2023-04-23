a67978acc38074e08a281ff250cde49df3c9c880
Rafael <soares815@hotmail.com>
2023-04-23 18:10:13 -0300

    created test class and generated jar

---
+++ src/test/java/org/example/structure/LeafTest.java
@@ -0,0 +1,12 @@
+package org.example.structure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeafTest {
+
+    @Test
+    void ask() {
+    }
+}
---
+++ src/test/java/org/example/structure/NodeTest.java
@@ -0,0 +1,76 @@
+package org.example.structure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+
+    @Test
+    void ask() {
+    }
+
+    @Test
+    void isThisSomething() {
+    }
+
+    @Test
+    void hasWindow() {
+    }
+
+    @Test
+    void getScanner() {
+    }
+
+    @Test
+    void getWindow() {
+    }
+
+    @Test
+    void getLeftNode() {
+    }
+
+    @Test
+    void getRightNode() {
+    }
+
+    @Test
+    void getFeature() {
+    }
+
+    @Test
+    void getPlate() {
+    }
+
+    @Test
+    void isLeft() {
+    }
+
+    @Test
+    void setScanner() {
+    }
+
+    @Test
+    void setWindow() {
+    }
+
+    @Test
+    void setLeftNode() {
+    }
+
+    @Test
+    void setRightNode() {
+    }
+
+    @Test
+    void setFeature() {
+    }
+
+    @Test
+    void setPlate() {
+    }
+
+    @Test
+    void setLeft() {
+    }
+}
